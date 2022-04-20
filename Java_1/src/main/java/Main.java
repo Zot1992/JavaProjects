64191c65f359bddf1c0f7c0a73122e357e4d42ad
ROMA <demon199202@yandex.ru>
2022-04-20 17:59:10 +0300

    демосерсия 2

--- Java_1/src/main/java/Main.java
+++ Java_1/src/main/java/Main.java
@@ -10,21 +10,26 @@ public class Main {
         File file = new File("C:/Users/ROMA/IdeaProjects/JavaProjects/products.txt");//создает текстовый файл
         System.out.println("Хотите создать файл?");
         if (sc.next().equals("yes")) {
-            BufferedWriter writer=new BufferedWriter(new FileWriter(file,true));
+            BufferedWriter writer=new BufferedWriter(new FileWriter(file));
             System.out.println("Файл создан");
             Scanner sc2 = new Scanner(System.in);
             System.out.println("Хотите добавить продукт?");
-            while (sc2.next().equals("yes")){
+            while (sc2.next().equals("yes")){ //цикл будет идти пока мы будем писать нужное слово которое задали
                 Scanner sc3 = new Scanner(System.in);
                 System.out.print("Введите наименование продукта: ");
-                String name = sc3.nextLine();
+                String name = sc3.nextLine();//Задает данные в строку который мы вводим в консоли
+                writer.write(name+'\t'); //добавляет введеную строку в текстовый файл и делает табуляцию
                 System.out.print("Введите цену продукта: ");
-                int price = sc3.nextInt();
-                System.out.print("Введите категорию продукта:");
+                String price = sc3.nextLine();
+                writer.write(price+'\t');
+                System.out.print("Введите категорию продукта: ");
                 String category = sc3.nextLine();
+                writer.write(category);
+                writer.write('\n');
+                System.out.println("Хотите добавить продукт?");
+                sc2.next();//вводим слово и от него зависит выйдем мы из цикла или начнем его сначала
             }
-
-            writer.close();
+            writer.close();//Закрывает поток ввода данных
         }
         else {
             System.out.println("Файл не создан");
