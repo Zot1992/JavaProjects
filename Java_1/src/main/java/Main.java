b7dd50e19bdf9f8cde5d44819a674e417c1d3ed1
ROMA <demon199202@yandex.ru>
2022-04-26 16:24:12 +0300

    релиз

--- Java_1/src/main/java/Main.java
+++ Java_1/src/main/java/Main.java
@@ -12,40 +12,47 @@ public class Main {
         int count=0; // считает колличесво товаров
         int sum=0;// сумма цен всех товаров
         int average_price=0;// средняя цена
-        //int [] arr=new int[count];//записывать преобразованные в int числа
         ArrayList<Integer>product=new ArrayList<>();//записывать преобразованные в int числа
         int priceint=0;// цена в виде int
         System.out.println("Хотите создать файл?");
-        if (sc.next().equals("yes")) {
+        if (sc.next().equals("да")) {
             BufferedWriter writer=new BufferedWriter(new FileWriter(file));
             System.out.println("Файл создан");
             Scanner sc2 = new Scanner(System.in);
+            Scanner sc3 = new Scanner(System.in);
+            System.out.println("По какой категории хотите узнать среднюю цену?");
+            String сategory_ap = sc3.nextLine();
             System.out.println("Хотите добавить продукт?");
-            while (sc2.next().equals("yes")){ //цикл будет идти пока мы будем писать нужное слово которое задали
-                Scanner sc3 = new Scanner(System.in);
+            while (sc2.next().equals("да")){ //цикл будет идти пока мы будем писать нужное слово которое задали
+                Scanner sc4 = new Scanner(System.in);
                 Product prod=new Product();
                 System.out.print("Введите наименование продукта: ");
                 String name=prod.getProduct();// переменной присваиваем раздел из объекта
-                name = sc3.nextLine();//Задает данные в строку который мы вводим в консоли
+                name = sc4.nextLine();//Задает данные в строку который мы вводим в консоли
                 writer.write(name+'\t'); //добавляет введеную строку в текстовый файл и делает табуляцию
                 System.out.print("Введите цену продукта: ");
                 String price=prod.getPrice();
-                price = sc3.nextLine();
-                priceint = Integer.parseInt(price);//метод parseInt преобразует строку в число
-                product.add(priceint);//добавляем преобразованное число в список
+                price = sc4.nextLine();
                 writer.write(price+'\t');
                 System.out.print("Введите категорию продукта: ");
                 String category=prod.getCategory();
-                category = sc3.nextLine();
+                category = sc4.nextLine();
                 writer.write(category);
+                if(category.equals(сategory_ap)){ //Если мы вводим нужную категорию, то строка преобразуется в число и добавляется в список
+                    priceint = Integer.parseInt(price);//метод parseInt преобразует строку в число
+                    product.add(priceint);//добавляем преобразованное число в список
+                    count++;
+                }
                 writer.write('\n');
                 System.out.println("Хотите добавить продукт?");
-                count++;
             }
-            Scanner sc4 = new Scanner(System.in);
-            System.out.println("По какой котегории хотите узнать среднюю цену?");
+            for (int i=0;i<product.size();i++){ //цикл для подсчета суммы по выбранной категории
+                sum+= product.get(i);
+            }
+
             average_price=sum/count;
-            System.out.println("среднее арифметическое по цене: "+average_price);
+            System.out.println("среднее арифметическое цена по заданной категории: "+average_price);
+            writer.write("среднее арифметическое цена по заданной категории: "+average_price);
 
             writer.close();//Закрывает поток ввода данных
         }
