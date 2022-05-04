71894fc048b8c59be84e8b69fdbcc0c1547c9dba
ROMA <demon199202@yandex.ru>
2022-05-05 01:38:01 +0300

    демоверсия 9

--- Java_1/src/main/java/Main.java
+++ Java_1/src/main/java/Main.java
@@ -10,11 +10,12 @@ public class Main {
     public static void main(String[] args) throws IOException {//IOException - Исключение ввода-вывода {
         Scanner sc = new Scanner(System.in);
         File file = new File("products.txt");//создает текстовый файл в корневой папке или использует существующий
-        float count = 0; // считает количество товаров
-        float sum = 0;// сумма цен всех товаров
+        float count = 0f; // считает количество товаров
+        float sum = 0f;// сумма цен всех товаров которые мы хотим добавить в файл
+        float sum2=0f;//сумма цен товаров которые уже были добавлены в файл
         float average_price = 0;// средняя цена
         ArrayList<Float> product = new ArrayList<>();//записывать преобразованные в int числа
-        float pricefloat = 0;// цена в виде int
+        float pricefloat = 0f;// цена в виде float
         System.out.println("Хотите создать файл?");
         if (file.isFile()) {System.out.println("Файл уже создан");}
         else if (sc.next().equals("да")) { //# если введу ДА - не сработает, нужно привести строки к КАПСУ или нижнему регистру, str.toLowerCase()/str.toUpperCase()
@@ -27,11 +28,11 @@ public class Main {
         BufferedWriter writer = new BufferedWriter(new FileWriter(file, true));//если дописываешь true, то дописывает файл, а не перезаписывает
         Scanner sc2 = new Scanner(System.in);
         Scanner sc3 = new Scanner(System.in);
-
+        System.out.println("По какой категории хотите узнать среднюю цену?");
+        String сategory_ap = sc3.nextLine();
         System.out.println("Хотите добавить продукт?");
         while (sc2.next().equals("да")) { //цикл будет идти пока мы будем писать нужное слово которое задали
             Scanner sc4 = new Scanner(System.in);
-            Product prod = new Product();
             writer.write(" ");
             System.out.print("Введите наименование продукта: ");
             String name;
@@ -45,18 +46,18 @@ public class Main {
             String category;
             category = sc4.nextLine();
             writer.write(category);
-            //#в итоге объект prod пустой и не содержит полей
-            //также необходимо считать уже имеющиеся данные в существующем файле
-            /*if (category.equals(сategory_ap)) { //Если мы вводим нужную категорию, то строка преобразуется в число и добавляется в список
+            if (category.equals(сategory_ap)) { //Если мы вводим нужную категорию, то строка преобразуется в число и добавляется в список
                 pricefloat = Float.parseFloat(price);
                 product.add(pricefloat);//добавляем преобразованное число в список
                 count++;
-            } */
+            }
             writer.write('\n');
             System.out.println("Хотите добавить продукт?");
         }
-        System.out.println("По какой категории хотите узнать среднюю цену?");
-        String сategory_ap = sc3.nextLine();
+
+        /*System.out.println("По какой категории хотите узнать среднюю цену?");
+        String сategory_ap = sc3.nextLine(); */
+
         FileReader fr=new FileReader(file);//считывает файл
         BufferedReader reader=new BufferedReader(fr);
         String line=reader.readLine();
@@ -66,12 +67,12 @@ public class Main {
             String Product=arr[0];
             Float Price=Float.parseFloat(arr[1]);
             String Category=arr[2];
-
+            Product prod = new Product(Product,Price,Category);
+            arrayProduct.add(prod);
             if (Category.equals(сategory_ap)) { //Если мы вводим нужную категорию, то строка преобразуется в число и добавляется в список
-                product.add(Price);//добавляем преобразованное число в список
+                sum2 += Price;
                 count++;
             }
-
             line=reader.readLine();
         }
         for (Product product2:arrayProduct){
@@ -80,7 +81,7 @@ public class Main {
         for (int i = 0; i < product.size(); i++) { //цикл для подсчета суммы по выбранной категории
             sum += product.get(i);
         }
-        average_price = sum / count;
+        average_price = (sum+sum2) / count;
         String average_price_str = Float.toString(average_price);
         System.out.println("среднее арифметическое цена по заданной категории: " + average_price_str);
         writer.write(average_price_str);
--- Java_1/src/main/java/Product.java
+++ Java_1/src/main/java/Product.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 public class Product {
     public String toString(){return product+"\t"+price+"\t"+category;}
 
-    public  String product="default";
-    private String price="default";
+    private  String product="default";
+    private Float price=0f; //если тип Float, то в конце числа добавляется буква f
     private  String category="default";
 
     public String getProduct(){return product;}
-    public String getPrice(){return price;}
+    public Float getPrice(){return price;}
     public String getCategory(){return category;}
 
     public Product(){};//конструктор по умолчанию
-    public Product(String p, String pr,String c){
+    public Product(String p, Float pr,String c){
         product=p;
         price=pr;
         category=c;
