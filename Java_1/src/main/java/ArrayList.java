e810320d74d7239e9dc7cb5814e62debb9ee28f1
ROMA <demon199202@yandex.ru>
2022-05-19 16:18:34 +0300

    fix 2

--- Java_1/src/main/java/ArrayList.java
+++ Java_1/src/main/java/ArrayList.java
@@ -9,8 +9,8 @@ public class ArrayList<E> {
     Помечено ключевым словом transient – поле не записывается в поток байт при применении стандартного алгоритма сериализации.
     Стоит отметить, что поле не отмечено ключевым словом private, а сделано это для того,
     чтобы облегчить доступ к этому полю из вложенных классов (например, SubList). */
-    private final Object[] DEFAULTCAPACITY_EMPTY_ELEMENTDATA =new Object[DEFAULT_CAPACITY];//Пустой массив по умолчанию
-    private final Object[] EMPTY_ELEMENTDATA = new Object[DEFAULT_CAPACITY];
+    private static final Object[] DEFAULTCAPACITY_EMPTY_ELEMENTDATA =new Object[DEFAULT_CAPACITY];//Пустой массив по умолчанию
+    private static final Object[] EMPTY_ELEMENTDATA = new Object[DEFAULT_CAPACITY];
 
 
     public int getSize() {return size;}
@@ -37,7 +37,7 @@ public class ArrayList<E> {
 
 
 
-    public Arrays.copyOf(elementData, newCapacity(minCapacity)){}
+   /* public Arrays.copyOf(elementData, newCapacity(minCapacity)){}
 
     private E[] grow() {return grow(size + 1);}
 
@@ -45,7 +45,7 @@ public class ArrayList<E> {
         int oldCapacity = elementData.length;//записываем в переменную текущий массив
         int newCapacity = oldCapacity + (oldCapacity >> 1); /*осуществляется вычисление нового размера массива. Он вычисляется по следующей формуле.
         >> 1 – побитовый сдвиг вправо на единицу (оператор, который уменьшает число до его половины). По сути, означает деление на 2 в степени 1.
-        Получается, что мы делим 10 на 2 и прибавляем 10. Итого, новая емкость массива равна 15, но так как мы добавляем 11 элемент, то 15+1 = 16.*/
+        Получается, что мы делим 10 на 2 и прибавляем 10. Итого, новая емкость массива равна 15, но так как мы добавляем 11 элемент, то 15+1 = 16.
 
 
 
@@ -54,19 +54,34 @@ public class ArrayList<E> {
     }
 
     private void add(E e, E[] elementData, int s){ /*Внутри (public boolean add) метода происходит вызов перегруженного варианта метода add(),
-    помеченный как private, который в свою очередь на вход принимает три параметра: добавляемый элемент, внутренний массив и его размер (size).*/
+    помеченный как private, который в свою очередь на вход принимает три параметра: добавляемый элемент, внутренний массив и его размер (size).
         if (size == elementData.length){elementData = grow();} /*В закрытом методе происходит проверка: если переданный параметр размера равен длине
-    внутреннего массива (то есть массив заполнен)*/
+    внутреннего массива (то есть массив заполнен)
         elementData[s] = e;
         size = s + 1;
-    }
+    } */
 
 
 
     public boolean add(E element){ //Классическое добавление элементов в списочный массив осуществляется с помощью перегруженных вариантов метода add().
-
+       try{ Object[] temp=elementData;
+        elementData = new Object[temp.length+1];//создаем массив размером на 1 больше
+        System.arraycopy(temp,0,elementData,0,temp.length); /*Метод для копирования из массива в массив.1.это то откуда будем копировать(temp).
+  2.это с какого элемента будем копировать(0).3.в каком будем копировать(elementData).4.с какого элемента(0).5. Сколько всего будем копировать(temp.length).*/
+        elementData[elementData.length-1]=element;//В конце массива присваиваем тот элемент, который хотим добавить.
         return true;
+       } catch (ClassCastException e){ e.printStackTrace(); } //если у нас к коде выше что-то пошло не так тогда мы вернем false.
+        return false;
     }
 
+    void remove(int index){ //удаляет указанный по индексу элемент из списка
+
+    for (int i=0;i<size-1;i++){
+        if (i==index){
+            elementData[i]=null;
+            size--;
+        }
+    }
+    }
 
 }
--- Java_1/src/main/java/Main.java
+++ Java_1/src/main/java/Main.java
@@ -1,12 +1,19 @@
-
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList <String> list = new ArrayList<>();
-        list.add("0");
-
-
+        ArrayList <Integer> list = new ArrayList<>();
+        int val;
+        int size;
+        Scanner sc=new Scanner(System.in);
+        size= sc.nextInt();
+        for (int i=0;i<size;i++){
+            val= sc.nextInt();
+            list.add(val);
+        }
     }
+    add(3);
+    remove(2);
 
 }
