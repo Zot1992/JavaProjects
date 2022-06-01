2bf4f9afecf87dfa07f54719f5589e9bebb65355
ROMA <demon199202@yandex.ru>
2022-06-01 15:28:30 +0300

    fix 2.5

--- Java_1/src/main/java/Main2.java
+++ Java_1/src/main/java/Main2.java
@@ -6,16 +6,21 @@ public class Main2 {
     public static void main(String[] args) {
 
         File mainfolder = new File("C://Users//ROMA//IdeaProjects//JavaProjects//Java_1//Директория");
+        File dir = new File("C://Users//ROMA//IdeaProjects//JavaProjects//Java_1//Директория//директория 1");
+        File dir2 = new File("C://Users//ROMA//IdeaProjects//JavaProjects//Java_1//Директория//директория1//директория 1.1");
+        File dir3 = new File("C://Users//ROMA//IdeaProjects//JavaProjects//Java_1//Директория//директория1//директория 1.1.1");
 
         int indent=1; // отступ
         Stack <String> st = new Stack<>();
-        st.push(mainfolder.getName());//добавляю название коренной папки в стек
+        //st.push(mainfolder.getName());//добавляю название коренной папки в стек
+        //st.push(dir.getName());
 
-        /*for (File file : mainfolder.listFiles()) {
+
+        for (File file : mainfolder.listFiles()) {
             if (file.isDirectory()) {// проверка файла на директорию
                 st.push(file.getName()); // если файл является директорией, то он добавляется в стек
             }
-        }*/
+        }
 
         if (!st.empty()) {
             for(int i = 0; i<indent; i++) {
---
+++ Java_1/src/main/java/Vertex.java
@@ -0,0 +1,9 @@
+public class Vertex {
+    public char name;
+    public boolean isisVisited;
+
+    public Vertex(char name){
+        this.name=name;
+        isisVisited=false;
+    }
+}
