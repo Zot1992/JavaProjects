4082bbd9cd463d4262954348fa3082948a20703b
ROMA <demon199202@yandex.ru>
2022-06-08 16:49:52 +0300

    fix 2.10

--- Java_1/src/main/java/Main2.java
+++ Java_1/src/main/java/Main2.java
@@ -3,7 +3,7 @@ import java.util.Stack;
 import java.lang.String;
 
 public class Main2 {
-    public static String FindControlSansRecursion(File root) {
+   /* public static String DFS(File root) {
         int indent = 1; // отступ
         Stack<String> stack = new Stack<String>();
         stack.push(root.getName());
@@ -12,19 +12,19 @@ public class Main2 {
             for (int i = 0; i < indent; i++) {
                 System.out.print("  ");
             }
-            String current = stack.pop();
+            String current = stack.pop(); //Выкидывает из стека название директории чтобы она была первой при выводе
             System.out.println(current);
             indent++; // увеличивает отступ
 
-            for (File file : root.listFiles());
-            {
+            for (File file : root.listFiles()) {
                 if (file.isDirectory()) {// проверка файла на директорию
-                    stack.push(file.getName());
+                    stack.push(file.getName()); // если файл является директорией, то он добавляется в стек
                 }
             }
         }
         return null;
-    }
+    } */
+
     public static void main(String[] args) {
 
         File mainfolder = new File("C://Users//ROMA//IdeaProjects//JavaProjects//Java_1//Директория");
@@ -32,6 +32,25 @@ public class Main2 {
         File dir2 = new File("C://Users//ROMA//IdeaProjects//JavaProjects//Java_1//Директория//директория1//директория 1.1");
         File dir3 = new File("C://Users//ROMA//IdeaProjects//JavaProjects//Java_1//Директория//директория1//директория 1.1.1");
 
+        int indent = 1; // отступ
+        Stack<String> stack = new Stack<>();
+        stack.push(mainfolder.getName());
+
+
+        while (!stack.empty()) {
+            for (int i = 0; i < indent; i++) {
+                System.out.print("  ");
+            }
+            String current = stack.pop(); //Выкидывает из стека название директории чтобы она была первой при выводе
+            System.out.println(current);
+            indent++; // увеличивает отступ
+
+            for (File file : mainfolder.listFiles()) {
+                if (file.isDirectory()) {// проверка файла на директорию
+                    stack.push(file.getName()); // если файл является директорией, то он добавляется в стек
+                }
+            }
+        }
         /*int indent=1; // отступ
         Stack <String> st = new Stack<>();
         //st.push(mainfolder.getName());//добавляю название коренной папки в стек
@@ -55,8 +74,9 @@ public class Main2 {
             }
         } */
 
-        String Print=FindControlSansRecursion(mainfolder);
-        System.out.println(Print);
-    }
+            //String Print=DFS(mainfolder);
+            //System.out.println(Print);
 
+
+    }
 }
