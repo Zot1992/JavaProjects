7e99db316582943e6dacf8d1ed428cbf1d67a7fc
ROMA <demon199202@yandex.ru>
2022-06-08 17:24:27 +0300

    fix 2.10

--- Java_1/src/main/java/Main2.java
+++ Java_1/src/main/java/Main2.java
@@ -51,32 +51,7 @@ public class Main2 {
                 }
             }
         }
-        /*int indent=1; // отступ
-        Stack <String> st = new Stack<>();
-        //st.push(mainfolder.getName());//добавляю название коренной папки в стек
-        //st.push(dir.getName());
-
-
-
-        for (File file : mainfolder.listFiles()) {
-            if (file.isDirectory()) {// проверка файла на директорию
-                st.push(file.getName()); // если файл является директорией, то он добавляется в стек
-            }
-        }
-
-        if (!st.empty()) {
-            for(int i = 0; i<indent; i++) {
-                System.out.print("  ");
-            }
-            for (int i = 0; i < st.size(); i++){
-                System.out.println(st.pop()); // выводит название директории и удаляет ее из стека
-                indent++; // увеличивает отступ
-            }
-        } */
-
-            //String Print=DFS(mainfolder);
-            //System.out.println(Print);
-
-
+        //String Print=DFS(mainfolder);
+        //System.out.println(Print);
     }
 }
