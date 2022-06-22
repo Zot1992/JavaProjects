6b6b17725e495a466f4b99560ac05a5e542e77a8
tfkfan <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>
2022-06-22 21:25:47 +0300

    fix

--- Java_1/src/main/java/Main2.java
+++ Java_1/src/main/java/Main2.java
@@ -1,26 +1,28 @@
 import java.io.File;
+import java.io.FileFilter;
+import java.util.Objects;
 import java.util.Stack;
 import java.lang.String;
 
 public class Main2 {
     public static void printDirTree(File root) {
-
         Stack<Directory> stack = new Stack<>();
         Directory dir_root = new Directory(root, 1);
         stack.push(dir_root);
 
         while (!stack.empty()) {
-            Directory current = stack.pop(); //Выкидывает из стека название директории чтобы она была первой при выводе
-            for (int i=0;i<current.getLevel();i++) {
-                System.out.print("-");
-            }
-            System.out.println(current.getFile());
+            final Directory current = stack.pop(); //Выкидывает из стека название директории чтобы она была первой при выводе
+            final File file = current.getFile();
+            final File[] directories = file.listFiles(File::isDirectory);
 
-            for (File file : current.getFile().listFiles()) {
+            for (int i = 0; i < current.getLevel(); i++)
+                System.out.print("\t");
 
-                Directory lev = new Directory(file, current.getLevel()+1);
-                stack.push(lev); // если файл является директорией, то он добавляется в стек
-            }
+            System.out.println(file);
+
+            if (!Objects.isNull(directories))
+                for (File dir : directories)
+                    stack.push(new Directory(dir, current.getLevel() + 1)); // если файл является директорией, то он добавляется в стек
         }
     }
 
