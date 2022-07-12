74245e8846fc266f60cbc6c4bcfa5277f2faacce
ROMA <demon199202@yandex.ru>
2022-07-12 16:18:13 +0300

    fix 3.12

--- Vector/src/test/java/VectorTest.java
+++ Vector/src/test/java/VectorTest.java
@@ -5,50 +5,50 @@ public class VectorTest {
 
     @org.junit.Test
     public void addVector() {
-        Vector v1 = new Vector(1, 2, 3);//создание вектора и его координат
-        Vector v2 = new Vector(1, 2, 3);
+        Vector v1 = new Vector(1, 0, 0);//создание вектора и его координат
+        Vector v2 = new Vector(1, 0, 0);
         Vector result = v1.addVector(v2);
         Assert.assertEquals(2, result.getX(),0.001 );
     }
 
     @org.junit.Test
     public void minusVector() {
-        Vector v1 = new Vector(1, 2, 3);//создание вектора и его координат
-        Vector v2 = new Vector(1, 2, 3);
+        Vector v1 = new Vector(1, 0, 0);//создание вектора и его координат
+        Vector v2 = new Vector(1, 0, 0);
         Vector result = v1.minusVector(v2);
         Assert.assertEquals(0, result.getX(),0.001);
     }
 
     @org.junit.Test
     public void lengthVector() {
-        Vector v1 = new Vector(1, 2, 3);//создание вектора и его координат
-        Vector v2 = new Vector(1, 2, 3);
+        Vector v1 = new Vector(1, 0, 0);//создание вектора и его координат
+        Vector v2 = new Vector(1, 0, 0);
         double result = v1.lengthVector(v2);
-        Assert.assertEquals(5, result,0.001);
+        Assert.assertEquals(1, result,0.001);
     }
 
     @org.junit.Test
     public void scalarProductVector() {
-        Vector v1 = new Vector(1, 2, 3);//создание вектора и его координат
-        Vector v2 = new Vector(1, 2, 3);
+        Vector v1 = new Vector(1, 0, 0);//создание вектора и его координат
+        Vector v2 = new Vector(1, 0, 0);
         double result = v1.scalarProductVector(v2);
-        Assert.assertEquals(5, result,0.001);
+        Assert.assertEquals(1, result,0.001);
     }
 
     @org.junit.Test
     public void normalization() {
-        Vector v1 = new Vector(1, 2, 3);//создание вектора и его координат
-        Vector v2 = new Vector(1, 2, 3);
+        Vector v1 = new Vector(1, 0, 0);//создание вектора и его координат
+        Vector v2 = new Vector(1, 0, 0);
         Vector result = v1.normalization(v2);
-        Assert.assertEquals(5, result.getX(),0.001);
+        Assert.assertEquals(1, result.getX(),0.001);
     }
 
     @org.junit.Test
     public void getting_an_angle() {
-        Vector v1 = new Vector(1, 2, 3);//создание вектора и его координат
-        Vector v2 = new Vector(1, 2, 3);
+        Vector v1 = new Vector(1, 0, 0);//создание вектора и его координат
+        Vector v2 = new Vector(1, 0, 0);
         double result = v1.getting_an_angle(v2);
-        Assert.assertEquals(5, result,0.001);
+        Assert.assertEquals(1, result,0.001);
     }
 
     @org.junit.Test
