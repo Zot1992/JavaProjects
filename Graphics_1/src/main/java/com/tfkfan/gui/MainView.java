f86d119fb432c0609f5dfbd721966b4ec23e4c26
ROMA <demon199202@yandex.ru>
2022-08-29 18:58:48 +0300

    задача на звезду 5.5

--- Graphics_1/src/main/java/com/tfkfan/gui/MainView.java
+++ Graphics_1/src/main/java/com/tfkfan/gui/MainView.java
@@ -1,3 +1,6 @@
+/*Написать графическую программу, выполняющую:
+        1) рисующую красный прямоугольник на черном экране */
+
 package com.tfkfan.gui;
 
 import com.vividsolutions.jts.geom.Coordinate;
@@ -8,6 +11,8 @@ public class MainView extends View {
     private Coordinate viewPoint;
     private double dx = 0.01d;//перемещение по оси х(по горизонтали)
     private double dy = 0.01d;//перемещение по оси y(по вертикали)
+    private double numRays=5;
+    private double deltaAngleR = 2*Math.PI / numRays;
 
     private int W = 87;
     private int A = 65;
@@ -15,8 +20,8 @@ public class MainView extends View {
     private int S = 83;
 
     public MainView() {
-        viewPoint = new Coordinate(0.5, 0.5);
-    } //начальное положение подвижной точки
+        viewPoint = new Coordinate(0.5, 0.5);//начальное положение подвижной точки
+    }
 
     @Override
     public void onKeyboardInput(int code) { //ОБРАБОТЧИК СОБЫТИЯ ВВОДА С КЛАВИАТУРЫ
@@ -38,11 +43,27 @@ public class MainView extends View {
     @Override
     protected void partialDisplay() { //Отображение подвижной и неподвижной фигуры
         glColor3f(2.0f, 0.0f, 0.0f);//цвет неподвижной фигуры
-        glBegin(GL_POLYGON);
-        glVertex2d(-0.5, -0.5);//положение нижней левой точки фигуры
-        glVertex2d(-0.5, 0.0);//положение верхней левой точки фигуры
-        glVertex2d(0.0, 0.0);//положение верхней правой точки фигуры
-        glVertex2d(0.0, -0.5);//положение нижней правой точки фигуры
+        //glBegin(GL_POLYGON);// чертит линии с заливкой
+        glBegin(GL_LINES);// чертит линии без заливки
+        /*glVertex2d(-0.5, -0.5);//положение нижней левой точки фигуры(v-ось x. v1-ось y)(декартовы координаты это координаты x,y с осями под прямым углом)
+        //glVertex2d(-0.5, 0.0);//положение верхней левой точки фигуры
+        //glVertex2d(0.0, 0.0);//положение верхней правой точки фигуры
+        //glVertex2d(0.0, -0.5);//положение нижней правой точки фигуры */ //код прямоугольника
+        /*glVertex2d(-0.25, -0.25); //код развернутого треугольника
+        glVertex2d(0.0, 0.0);
+        glVertex2d(-0.5, 0.0);*/
+
+        glVertex2d(0.0, .8); // код звезды (неправильный)
+        glVertex2d(0.6, -0.7);
+        glVertex2d(-0.8, 0.1);
+        glVertex2d(0.8, 0.1);
+        glVertex2d(-0.6, -0.7);
+        glVertex2d(0.0, 0.8);
+
+
+
+
+
         glEnd();
 
         glColor3f(0.2f, 0.2f, 0.2f);//цвет подвижной точки
