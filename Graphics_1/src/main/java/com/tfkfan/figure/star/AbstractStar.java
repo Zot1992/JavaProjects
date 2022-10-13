9387ccccba5e81b3b29579a34a58834653f75af1
tfkfan <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>
2022-10-13 21:10:51 +0300

    DIBILISM

--- Graphics_1/src/main/java/com/tfkfan/figure/star/AbstractStar.java
+++ Graphics_1/src/main/java/com/tfkfan/figure/star/AbstractStar.java
@@ -2,44 +2,55 @@ package com.tfkfan.figure.star;
 
 import com.vividsolutions.jts.geom.Coordinate;
 
-public abstract class AbstractStar implements Star{
+public abstract class AbstractStar implements Star {
     protected int vertices;
     protected double size;
     protected double deltaAngle;
     protected Coordinate[] starPoints;
     protected Coordinate center;
 
-    public AbstractStar(double size, int vertices){
-        this.starPoints = new Coordinate [vertices];
+    public AbstractStar(double size, int vertices) {
+        this.starPoints = new Coordinate[vertices];
         this.setVertices(vertices);
         this.setSize(size);
     }
-    public AbstractStar(Coordinate [] starPoints,int vertices) {
+
+    public AbstractStar(Coordinate[] starPoints, double size, int vertices) {
         this.starPoints = starPoints;
         this.setVertices(vertices);
+        this.setSize(size);
     }
 
     @Override
-    public void setVertices(int vertices){
-        this.vertices=vertices;
-        this.deltaAngle=2 * Math.PI / vertices;
+    public void setVertices(int vertices) {
+        this.vertices = vertices;
+        this.deltaAngle = 2 * Math.PI / vertices;
     }
+
     @Override
-    public void setSize(double size){this.size=size;}
+    public void setSize(double size) {
+        this.size = size;
+    }
 
     @Override
     public Coordinate center() {
         return center;
     }
+
     @Override
     public Coordinate[] starPoints() {
         return starPoints;
     }
 
     @Override
-    public int vertices(){ return vertices;}
+    public int vertices() {
+        return vertices;
+    }
+
     @Override
-    public double size(){return size;}
+    public double size() {
+        return size;
+    }
 
     @Override
     public void move(double dx, double dy) {
@@ -47,8 +58,8 @@ public abstract class AbstractStar implements Star{
             starPoint.y += dy;
             starPoint.x += dx;
         }
-        center.x +=dx;
-        center.y +=dy;
+        center.x += dx;
+        center.y += dy;
     }
 }
 
--- Graphics_1/src/main/java/com/tfkfan/figure/star/LinedStar.java
+++ Graphics_1/src/main/java/com/tfkfan/figure/star/LinedStar.java
@@ -1,11 +1,12 @@
 package com.tfkfan.figure.star;
 
 import com.vividsolutions.jts.geom.Coordinate;
+
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.opengl.GL11.glBegin;
 
 
-public class LinedStar extends AbstractStar{
+public class LinedStar extends AbstractStar {
 
     private Coordinate[] starPoints;
     public int vertices = 5;
@@ -16,23 +17,19 @@ public class LinedStar extends AbstractStar{
     public double speed = 0.005;
     public double alfa = 0.05;
 
-    public LinedStar(double size, int vertices){
-        super(size,vertices);
+    public LinedStar(double size, int vertices) {
+        super(size, vertices);
     }
 
-    public LinedStar(Coordinate [] starPoints,int vertices){
-        super(starPoints,vertices);
+    public LinedStar(Coordinate[] starPoints, double size, int vertices) {
+        super(starPoints, size, vertices);
     }
 
-    public LinedStar(Coordinate center,double alfa){
-        super(center,alfa);
+    public void drawStar() {
+        drawStar(makeStar(size, vertices));
     }
 
-
-
-    public  void drawStar(){drawStar(makeStar(size,vertices));}
-
-    public  Coordinate[] makeStar(double size, int vertices) { //код на звезды(позиции точек)
+    public Coordinate[] makeStar(double size, int vertices) { //код на звезды(позиции точек)
         Coordinate[] points = new Coordinate[vertices];
         double x, y;
         double deltaAngleR = 2 * Math.PI / vertices;//нахождение угла для звезды.2*Math.PI-что бы получить 360 градусов.
@@ -44,16 +41,17 @@ public class LinedStar extends AbstractStar{
         }
         return points;
     }
-        public  void drawStar (Coordinate[]points){ //код на звезды(смещение точек и отрисовка их на экране)
-            glBegin(GL_LINE_LOOP);//Рисуется ломаная, причем ее последняя точка соединяется с первой.
-            int vertices = points.length;
-            for (int i = 0; i < points.length; i++) {
-                int shift = (i * 2) % vertices;   //Точки выводим со смещением на 2
-                glVertex2d(points[shift].x, points[shift].y);//object[i].x только так можно вызвать из массива объектов нужный метод
-            }
 
-            glEnd();// конец отрисовки. Работает в паре с glBegin
+    public void drawStar(Coordinate[] points) { //код на звезды(смещение точек и отрисовка их на экране)
+        glBegin(GL_LINE_LOOP);//Рисуется ломаная, причем ее последняя точка соединяется с первой.
+        int vertices = points.length;
+        for (int i = 0; i < points.length; i++) {
+            int shift = (i * 2) % vertices;   //Точки выводим со смещением на 2
+            glVertex2d(points[shift].x, points[shift].y);//object[i].x только так можно вызвать из массива объектов нужный метод
         }
+
+        glEnd();// конец отрисовки. Работает в паре с glBegin
+    }
 
     @Override //позволяет изменить параметры метода
     public void draw() {
@@ -62,14 +60,14 @@ public class LinedStar extends AbstractStar{
         glEnd();
     }
 
-    protected void rotateStar(double alfa){//функция для вращения звезды
+    protected void rotateStar(double alfa) {//функция для вращения звезды
         double cos = Math.cos(alfa);
         double sin = Math.sin(alfa);
         for (Coordinate point : starPoints) { // тоже самое что for(int i=0;i<starPoints;i++)
-            double ox = point.x,oy = point.y;
-            point.x = ((ox - center.x)*cos - (oy - center.y)*sin + center.x);//Формула матрицы поворота в двумерном пространстве.
+            double ox = point.x, oy = point.y;
+            point.x = ((ox - center.x) * cos - (oy - center.y) * sin + center.x);//Формула матрицы поворота в двумерном пространстве.
             // Поворот выполняется путём умножения матрицы поворота на вектор-столбец, описывающий вращаемую точку
-            point.y = ((ox - center.x)*sin + (oy - center.y)*cos + center.y);
+            point.y = ((ox - center.x) * sin + (oy - center.y) * cos + center.y);
         }
     }
 }
