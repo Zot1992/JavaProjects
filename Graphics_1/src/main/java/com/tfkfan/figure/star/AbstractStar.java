696ac63c031ec42e56f2d1211412e3f882c8673f
tfkfan <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>
2022-10-13 19:28:30 +0300

    DIBILISM

--- Graphics_1/src/main/java/com/tfkfan/figure/star/AbstractStar.java
+++ Graphics_1/src/main/java/com/tfkfan/figure/star/AbstractStar.java
@@ -5,10 +5,6 @@ import com.vividsolutions.jts.geom.Coordinate;
 public abstract class AbstractStar implements Star{
     protected int vertices;
     protected double size;
-    protected double begin;
-    protected double end;
-    protected double speed;
-    protected double alfa;
     protected double deltaAngle;
     protected Coordinate[] starPoints;
     protected Coordinate center;
@@ -23,16 +19,6 @@ public abstract class AbstractStar implements Star{
         this.setVertices(vertices);
     }
 
-    public AbstractStar(){
-        this.center=new Coordinate();
-    }
-
-    public AbstractStar(Coordinate center,double alfa){
-        this.center=center;
-        this.setAlfa(alfa);
-    }
-
-
     @Override
     public void setVertices(int vertices){
         this.vertices=vertices;
@@ -40,14 +26,7 @@ public abstract class AbstractStar implements Star{
     }
     @Override
     public void setSize(double size){this.size=size;}
-    @Override
-    public void setBegin(double begin){this.begin=begin;}
-    @Override
-    public void setEnd(double end){this.end=end;}
-    @Override
-    public void setSpeed(double speed){this.speed=speed;}
-    @Override
-    public void setAlfa(double alfa){this.alfa=alfa;}
+
     @Override
     public Coordinate center() {
         return center;
@@ -60,16 +39,7 @@ public abstract class AbstractStar implements Star{
     @Override
     public int vertices(){ return vertices;}
     @Override
-    public double begin(){return begin;}
-    @Override
     public double size(){return size;}
-    @Override
-    public double end(){return end;}
-    @Override
-    public double speed(){return speed;}
-    @Override
-    public double alfa(){return alfa;}
-
 
     @Override
     public void move(double dx, double dy) {
--- Graphics_1/src/main/java/com/tfkfan/figure/star/Star.java
+++ Graphics_1/src/main/java/com/tfkfan/figure/star/Star.java
@@ -4,18 +4,10 @@ import com.tfkfan.figure.Figure;
 import com.vividsolutions.jts.geom.Coordinate;
 
 public interface Star extends Figure { //полностью абстрактный класс который имеет методы без тел
-    public void setVertices(int vertices);
-    public void setSize(double size);
-    public void setBegin(double begin);
-    public void setEnd(double end);
-    public void setSpeed(double speed);
-    public void setAlfa(double alfa);
+    void setVertices(int vertices);
+    void setSize(double size);
 
     double size();
-    double begin();
-    double end();
-    double speed();
-    double alfa();
     int vertices();
 
     Coordinate center();
